80a8fed4e35579f9d76a680075d16e930b62d76c
redcricket <red.cricket.blog@gmail.com>
2019-12-15 10:51:10 -0800

    good stopping point.

--- README.md
+++ README.md
@@ -15,3 +15,81 @@ $ npm start
 
 I go the above instructions from https://create-react-app.dev/docs/getting-started/#creating-a-typescript-app
 
+## Routes
+
+I am trying to figure out how to do this with Typescript or if I need to or not.
+
+https://create-react-app.dev/docs/adding-a-router
+
+This was the first result if I web searched 'typescript router react'.
+
+https://www.pluralsight.com/guides/react-router-typescript
+
+As per the above docs I executed:
+
+```
+$ npm install --save react-router-dom
+```
+
+I guess I need to do this too:
+
+```bash
+$ npm install @types/react-router-dom
+```
+
+I also changed `src/App.jsx` to 
+
+```typescript
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, RouteComponentProps } from 'react-router-dom';
+
+import logo from './logo.svg';
+import './App.css'; import {is} from "@babel/types"; import {exact} from "prop-types";
+
+
+function Index() { return <h2>Home</h2>; }
+type TParams = { id: string };
+
+function Product({ match }: RouteComponentProps<TParams>) {
+  return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
+
+const App: React.FC = () => {
+  return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li> <Link to="/">Home</Link> </li>
+              <li> <Link to="/products/1">First Product</Link> </li>
+              <li> <Link to="/products/2">Second Product</Link> </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/products/:id" component={Product} />
+        </div>
+      </Router>
+  );
+}
+
+export default App;
+```
+
+After doing that I tested and it worked.  So I went back to:
+
+https://medium.com/better-programming/building-basic-react-authentication-e20a574d5e71
+
+The above article states we need to execute:
+
+```
+npm install --save styled-components react-router-dom axios
+```
+
+we already install react-router-dom so I executed:
+
+```
+npm install --save styled-components axios
+```
+
+I added the file `src/PrivateRoute.jsx` based on the [src/PrivateRoute.js](https://gist.github.com/DennyScott/a1f00ac31b9b14bbd889f4b39a970e81/raw/67ed2d00e15324da33588402b9966157028c41d7/react-hook-auth-block6.jsx)
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, RouteComponentProps } from 'react-router-dom';
+import {AuthContext, useAuth} from "./context/auth";
 import './App.css';
+import PrivateRoute from "./PrivateRoute";
+import logoImg from "./img/logo.svg";
+import { Card, Logo, Form, Input, Button } from "./components/AuthForm";
 
-const App: React.FC = () => {
+// function oldIndex() { return <h2>Home</h2>; }
+function Index() {
+    const isAuthenticated = useAuth();
+    return isAuthenticated ? ( <h2>Home you are logged in.</h2> ) : (
+        <Router>
+            <div>
+                <h2>Home you not logged in.</h2>
+                <nav>
+                    <ul>
+                        <li> <Link to="/login">Login</Link> </li>
+                        <li> <Link to="/signup">Sign Up</Link> </li>
+                    </ul>
+                </nav>
+
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+            </div>
+        </Router>
+    );
+}
+type TParams = { id: string };
+
+function Product({ match }: RouteComponentProps<TParams>) {
+  return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
+
+function Admin (props: RouteComponentProps<TParams>) {
+  console.log(props);
+  return <h2>This is the {props.location.pathname} page.</h2>;
+}
+
+const Signup: React.FunctionComponent = () => {
+    return (
+        <Card>
+            <Logo src={logoImg} />
+            <Form>
+                <Input type="email" placeholder="email" />
+                <Input type="password" placeholder="password" />
+                <Input type="password" placeholder="password again" />
+                <Button>Sign Up</Button>
+            </Form>
+            <Link to="/login">Already have an account?</Link>
+        </Card>
+    );
+}
+
+const Login: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Card>
+        <Logo src={logoImg} />
+        <Form>
+          <Input type="email" placeholder="email" />
+          <Input type="password" placeholder="password" />
+          <Button>Sign In</Button>
+        </Form>
+        <Link to="/signup">Don't have an account?</Link>
+      </Card>
   );
 }
+
+
+const App: React.FunctionComponent = () => {
+  return (
+      <AuthContext.Provider value={false}>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li> <Link to="/">Home</Link> </li>
+                <li> <Link to="/products/1">First Product</Link> </li>
+                <li> <Link to="/products/2">Second Product</Link> </li>
+                <li> <Link to="/admin">Admin Page</Link> </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/products/:id" component={Product} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute path="/admin" component={Admin} />
+          </div>
+        </Router>
+      </AuthContext.Provider>
+  );
+};
 
 export default App;
